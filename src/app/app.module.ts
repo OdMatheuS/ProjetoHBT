547501efe67a2ce91dc1e30b464dca6a4912657e
OdMatheuS <matheusnagot@hotmail.com>
2020-07-11 21:34:19 -0300

    Criação dos Componentes para listar e contar os usuários do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserCountComponent } from './user-count/user-count.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserCountComponent
   ],
   imports: [
     BrowserModule,
